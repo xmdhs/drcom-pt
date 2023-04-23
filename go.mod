9e7d4463161097ee3f55f6bab2feb63c062d1733
xmdhs <xmdhss@gmail.com>
2023-04-23 15:16:51 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xmdhs/drcom-pt
+
+go 1.20
+
+require github.com/avast/retry-go/v4 v4.3.4
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/avast/retry-go/v4"
+)
+
+var (
+	user     string
+	pass     string
+	authAddr string
+)
+
+func init() {
+	flag.StringVar(&user, "u", "", "")
+	flag.StringVar(&pass, "p", "", "")
+	flag.StringVar(&authAddr, "a", "", "")
+	flag.Parse()
+}
+
+func main() {
+	cxt := context.Background()
+	c := &http.Client{Timeout: 5 * time.Second}
+	for {
+		err := retry.Do(func() error {
+			return checkWeb(cxt, c)
+		}, getRetryOpts(cxt, 5)...)
+		if err == nil {
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		err = retry.Do(func() error {
+			return login(cxt, c, user, pass, authAddr)
+		}, getRetryOpts(cxt, 5)...)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func checkWeb(cxt context.Context, c *http.Client) error {
+	req, err := http.NewRequestWithContext(cxt, "GET", "https://connect.rom.miui.com/generate_204", nil)
+	if err != nil {
+		return fmt.Errorf("checkWeb: %w", err)
+	}
+	reps, err := c.Do(req)
+	if reps != nil {
+		defer reps.Body.Close()
+	}
+	if err != nil {
+		return fmt.Errorf("checkWeb: %w", err)
+	}
+	_, err = io.Copy(io.Discard, reps.Body)
+	if err != nil {
+		return fmt.Errorf("checkWeb: %w", err)
+	}
+	return nil
+}
+
+func login(cxt context.Context, c *http.Client, user, pass, authAddr string) error {
+	v := url.Values{}
+	v.Set("DDDDD", user)
+	v.Set("upass", pass)
+	v.Set("0MKKey", "0123456789")
+	v.Set("ver", "1.3.5.201712141.P.W.A")
+	req, err := http.NewRequestWithContext(cxt, "POST", "http://172.17.100.100:801/eportal/?c=ACSetting&a=Login&jsVersion=3.0&login_t=2", strings.NewReader(v.Encode()))
+	if err != nil {
+		return fmt.Errorf("login: %w", err)
+	}
+	req.Header.Set("User-Agent", "DrCOM-HTTPCLIENT")
+	req.Header.Set("Accept-Language", "zh-CN")
+	req.Header.Set("Charset", "utf-8")
+	req.Header.Set("Connection", "Close")
+
+	resp, err := c.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+
+	}
+	if err != nil {
+		return fmt.Errorf("login: %w", err)
+	}
+	return nil
+}
+
+func getRetryOpts(cxt context.Context, attempts uint) []retry.Option {
+	return []retry.Option{
+		retry.Attempts(attempts),
+		retry.LastErrorOnly(true),
+		retry.MaxDelay(20 * time.Minute),
+		retry.Context(cxt),
+		retry.OnRetry(func(n uint, err error) {
+			log.Printf("retry %d: %v", n, err)
+		}),
+	}
+}
